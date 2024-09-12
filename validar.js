b1508a932743392cff1a5f18b664cd9abf524830
PedPesVel <pescador.velazquez.pedro@gmail.com>
2024-09-11 19:49:47 -0600

    Repertorio

---
+++ validar.js
@@ -0,0 +1,39 @@
+function validar(form){
+
+    //Validar Nombre
+    let nombre = form.nombre.value.trim()
+    
+    if(nombre==""){
+        alert("Por favor, ingrese su nombre")
+        return false
+    }
+    
+    let edad = form.edad.value.trim()
+    
+    alert(edad)
+    alert(nombre)
+    if(edad === "" || isNaN(edad) || edad<=0){
+        console.log("Entró")
+        alert("Por favor, ingrese una edad válida")
+        return false
+    }
+    
+    let sexo = form.sexo.value
+    
+    if(sexo == ""){
+        alert("Por favor, selecciona un sexo")
+        return false
+    }
+    
+    let deporte = form.deporte.value
+    
+    if(deporte === "ninguno"){
+        alert("Por favor, seleccione un deporte")
+        return false
+    }
+    
+    // Si todo es válido
+    return false;
+    
+    }
+    
